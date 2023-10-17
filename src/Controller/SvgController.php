96b953e3612a4fa7ff78aff42904ba1c1da1ebb5
Matthias Bendel <m.bendel@andagon.com>
2023-10-17 13:56:38 +0200

    vote

--- src/Controller/SvgController.php
+++ src/Controller/SvgController.php
@@ -7,6 +7,8 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use DateTime;
+
 class SvgController extends AbstractController
 {
     #[Route('/svg', name: 'app_svg')]
@@ -20,12 +22,35 @@ class SvgController extends AbstractController
         ]);
     }
 
+    #[Route('/vote/{vote}/{item}', name: 'app_vot')]
+    public function vote($vote, $item):  JsonResponse
+    {
+        if (in_array($vote, ['accept', 'tolerate', 'ignore', 'decline', 'select'])) {
+            $file = 'test.json';
+            $str = file_get_contents($file);
+            $json = json_decode($str, true); 
+            $time = time();
+            $json[$time] = array("vote" => $vote, "item" => $item);
+            file_put_contents($file, json_encode($json));
+            
+            return $this->json([
+                'message' => 'Voted '. $vote .' for ' . $item . ' (at ' . date('m/d/Y h:i:s a', $time) . '!'
+            ]);
+        }
+        return $this->json([
+            'error' => 'Invalid vote: ' . $vote
+        ]);
+    }
+
     #[Route('/json', name: 'app_json')]
     public function getJson(): JsonResponse
     {
+        $str = file_get_contents('http://example.com/example.json/');
+        $json = json_decode($str, true); 
         return $this->json([
             'message' => 'Welcome to your json controller!',
             'path' => 'src/Controller/SvgController.php',
+            'content' => $json
         ]);
     }
 }
