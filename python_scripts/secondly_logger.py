9ca7dc3b93162f9b4cbf9ef40dbe12d9f94dca32
matthias <matthiasbendel@gmail.com>
2024-01-12 17:37:54 +0100

    call secondly_logger.py minutely

--- python_scripts/secondly_logger.py
+++ python_scripts/secondly_logger.py
@@ -1,5 +1,17 @@
 import logging
 
+import time
+
+
+def log_secondly_for_one_minute():
+    # Set the end time for the script to run for 1 minute (60 seconds)
+    end_time = time.time() + 60
+
+    # Execute the "Hello, World!" message every second for one minute
+    while time.time() < end_time:
+        log_everything()
+        time.sleep(1)  # Pause for 1 second
+
 
 def log_everything():
     # Create and configure logger
